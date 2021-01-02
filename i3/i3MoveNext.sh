6bbbd1521768af8158b698c3b57a747cd892d358
drianclark <dreiclark@gmail.com>
2021-01-02 13:14:37 +0000

    modified i3MoveNext script

--- i3/i3MoveNext.sh
+++ i3/i3MoveNext.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 wsNext=$(( $( i3-msg -t get_workspaces | jq '.[] | select(.focused).num' ) + $1))
-i3-msg move container to workspace $wsNext
-i3-msg workspace $wsNext
+wsNextName=$(i3-msg -t get_workspaces | jq ".[] | select(.num == $wsNext).name")
+
+if [ -z "$wsNextName" ]; then
+    i3-msg move container to workspace number "$wsNext"
+    i3-msg workspace number "$wsNext"
+else
+    i3-msg move container to workspace "$wsNextName"
+    i3-msg workspace "$wsNextName"
+fi
